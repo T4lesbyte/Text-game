3d104f3e53316bca7ab3de6c8026d98a158c08cc
Julio Vasconcelos <110854782+JulioPVasconcelos@users.noreply.github.com>
2024-08-27 21:25:13 -0300

    Update CÓDIGO PRINCIPAL .c
    Tirei o if da função morreu()
    Tirei o DO da função morreu()
    
    tem bug na função comeco()

--- CÓDIGO PRINCIPAL .c
+++ CÓDIGO PRINCIPAL .c
@@ -8,85 +8,68 @@ char nome[40] ;
 int primeiro, seg, ter, quar, quinto, recomeco;
 bool a; // começa como false
 
-
-
-
 main() {
 
-setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
 
-printf ("Me conte seu nome, aventureiro \n");
+    printf ("Me conte seu nome, aventureiro \n");
     gets (nome);
     printf ("Seja bem-vindo, %s \n",nome);
 
-comeco();
+    comeco();
+    morreu();
 
-if(a == true ) {
+} // MAIN
 
-
-
-do {
+int morreu(){
 
     printf ("Você morreu! \n");
     printf ("Deseja tentar novamente? \n");
     printf ("(1)SIM   (2)NÃO \n");
     scanf ("%i", &recomeco);
 
-        switch(recomeco){
-    case 1:
-        printf ("\n");
-        comeco();
-        a = 0;
-        recomeco = 0;
-    break;
+    switch(recomeco){
+        case 1:
+            printf ("\n");
+            comeco();
+            recomeco = 0;
+        break;
 
-    case 2:
-        printf ("Covarde \n");
-        recomeco = 0;
-    break;
-    } //SWITCH
-} // DO
-while(recomeco == 1);
+        case 2:
+            printf ("Covarde \n");
+            recomeco = 1;
+            exit(0);
+        break;
+            } //SWITCH
 
-
+        while(recomeco == 1);
 
 
-} // IF
 
-} // MAIN
+} // MORREU
 
 int comeco() {
 
     //------------
+    //a = false;
 
     printf ("Você acorda no chão de uma caverna fria \n");
     printf ("Ao seu redor é possível ver alguns esqueletos, uma tocha com uma brasa ainda brilhante, e uma pedra achatada \n");
     printf ("Qual deles deseja pegar? (1)Osso (2)Tocha (3)Pedra \n");
     scanf ("%i", &primeiro);
 
-switch (primeiro) {
+    switch (primeiro) {
+        case 1:
+            printf ("Você agarra o fêmur de um esqueleto qualquer, parece que pode servir como uma boa arma \n");
+            a = false; // 0
+        break;
 
-case 1:
-    printf ("Você agarra o fêmur de um esqueleto qualquer, parece que pode servir como uma boa arma \n");
-    a = false; // 0
-break;
+        case 2:
+            printf ("broxa \n");
+            a = true; // 1
+            morreu();
+        break;
 
-case 2:
-    printf ("broxa \n");
-    a = true; // 1
-break;
-
-
-
-
-
-} // SWITCH
-
-
+    } // SWITCH
 
 } // INT COMECO
-
-
-
-
-
